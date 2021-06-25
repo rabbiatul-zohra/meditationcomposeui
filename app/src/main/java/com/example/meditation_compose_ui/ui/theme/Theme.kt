f31dbf3ce750850dc13794d5af8776169fc3107f
Rabbia <rabbia.tul-zohra@bbc.co.uk>
2021-06-25 10:47:16 +0100

    Initial commit with new resources

---
+++ app/src/main/java/com/example/meditation_compose_ui/ui/theme/Theme.kt
@@ -0,0 +1,14 @@
+package com.example.meditation_compose_ui.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MeditationUITheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+    MaterialTheme(
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
