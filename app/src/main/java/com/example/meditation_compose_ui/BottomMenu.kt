d5e8ab21a6d0178e00a1ccbbb9c52608b4b3af9c
Rabbia <rabbia.tul-zohra@bbc.co.uk>
2021-06-25 13:07:26 +0100

    Added ui for home screen and updated compose to beta 09 in order to resolve compiler errors

---
+++ app/src/main/java/com/example/meditation_compose_ui/BottomMenu.kt
@@ -0,0 +1,8 @@
+package com.example.meditation_compose_ui
+
+import androidx.annotation.DrawableRes
+
+data class BottomMenu(
+    val title: String,
+    @DrawableRes val iconId: Int
+)
--- app/src/main/java/com/example/meditation_compose_ui/MainActivity.kt
+++ app/src/main/java/com/example/meditation_compose_ui/MainActivity.kt
@@ -3,36 +3,23 @@ package com.example.meditation_compose_ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.meditation_compose_ui.ui.HomeScreen
 import com.example.meditation_compose_ui.ui.theme.MeditationUITheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MeditationUITheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                HomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MeditationUITheme {
-        Greeting("Android")
     }
 }
